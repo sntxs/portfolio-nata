a7982b26de86df4c34e4aca81db0e883bf71d1c7
sntxs <natanrodrigues649@gmail.com>
2024-12-03 14:33:45 -0400

    Remove BackgroundBubbles component and its references from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="bg-gray-100 min-h-screen relative overflow-hidden">
-    <BackgroundBubbles />
     <NavBar />
     <div class="container mx-auto px-4 relative">
       <Home />
@@ -17,5 +16,4 @@ import Home from './components/Home.vue'
 import About from './components/About.vue'
 import Skills from './components/Skills.vue'
 import Contact from './components/Contact.vue'
-import BackgroundBubbles from './components/BackgroundBubbles.vue'
 </script>
